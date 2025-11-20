3d413fd04027afcbe4da7ed2072646d4e754abfc
Sana Arah <sanaarah2006.email@example.com>
2025-11-20 13:59:37 +0530

    Organized project into frontend & backend + added backend structure

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -4,8 +4,7 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   image: { type: String },
-  description: { type: String },
   category: { type: String },
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model("Product", productSchema);
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/productModel");
 
-// ✅ Get all products
+//  Get all products
 router.get("/", async (req, res) => {
   try {
     const products = await Product.find();
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-// ✅ Add a new product
+
 router.post("/", async (req, res) => {
   try {
     const newProduct = new Product(req.body);
